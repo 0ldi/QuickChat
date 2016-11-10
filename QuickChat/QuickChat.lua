c5a73bee18b3ebc09e3ecee8bf577fb27d08082b
0ldi <0ldi@users.noreply.github.com>
2016-11-10 08:24:43 +0300

    Add files via upload

--- QuickChat/QuickChat.lua
+++ QuickChat/QuickChat.lua
@@ -1,11 +1,14 @@
 BINDING_HEADER_QuickChat="QuickChat Functions";
-BINDING_NAME_off = "Officer's Chat Window /o";
-BINDING_NAME_gui = "Guild Chat Window /g";
-BINDING_NAME_par = "Party Chat Window /p";
-BINDING_NAME_rai = "Raid Chat Window /raid";
+BINDING_NAME_whi = "Whisper Chat Window /w ";
 BINDING_NAME_say = "Say Chat Window /say";
 BINDING_NAME_yel = "Yell Chat Window /yell";
-BINDING_NAME_whi = "Whisper Chat Window /w";
+BINDING_NAME_emo = "Emote Chat Window /e";
+BINDING_NAME_par = "Party Chat Window /p";
+BINDING_NAME_rai = "Raid Chat Window /raid";
+BINDING_NAME_war = "Raid Warning Chat Window /rw";
+BINDING_NAME_bat = "Battleground Chat Window /bg";
+BINDING_NAME_gui = "Guild Chat Window /g";
+BINDING_NAME_off = "Officer's Chat Window /o";
 BINDING_NAME_one = "Chat Window /1";
 BINDING_NAME_two = "Chat Window /2";
 BINDING_NAME_thr = "Chat Window /3";
@@ -15,6 +18,5 @@ BINDING_NAME_six = "Chat Window /6";
 BINDING_NAME_sev = "Chat Window /7";
 BINDING_NAME_eig = "Chat Window /8";
 BINDING_NAME_nin = "Chat Window /9";
-
 
 
